af6f53a0883badd4c0383a10a6dc8616851d2bc1
Lucas Arruda <lucasarruda@gmail.com>
2018-04-05 16:54:27 -0300

    Create malicious usar as an example of how a user could explore a lack of validation on input

--- migrations/001-create.sql
+++ migrations/001-create.sql
@@ -6,6 +6,7 @@ CREATE TABLE User (name TEXT, login TEXT, password TEXT);
 
 INSERT INTO User (name, login, password) VALUES ('Cool Admin', 'admin@site.com', 'pass');
 INSERT INTO User (name, login, password) VALUES ('Other User', 'user@site.com', 'a$1sB!095$21!');
+INSERT INTO User (name, login, password) VALUES ("<script>alert('I hacked the system')</script>", 'login@hacker.com', '133t')
 
 --------------------------------------------------------------------------------
 -- Down
